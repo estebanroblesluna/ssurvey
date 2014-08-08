2da72ac91dbeac07dfb6fb4cc7311c329df460f7
jspurim <jspurim@gmail.com>
2014-08-08 09:53:32 -0300

    Project structure. Issue #1.

---
+++ modules/ssurvey-webapp/src/main/java/com/ssurvey/web/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.ssurvey.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView showHomePage(){
+		ModelAndView mv = new ModelAndView("home");
+		return mv;
+	}
+}
