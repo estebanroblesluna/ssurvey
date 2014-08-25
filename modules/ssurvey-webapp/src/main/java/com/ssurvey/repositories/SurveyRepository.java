0c345bbec2e43f5dd43c5f9fc41b4e4ed9170196
MartinAparicioPons <martin.aparicio.pons@gmail.com>
2014-08-25 09:32:09 -0300

    Add backend for questions.

--- modules/ssurvey-webapp/src/main/java/com/ssurvey/repositories/SurveyRepository.java
+++ modules/ssurvey-webapp/src/main/java/com/ssurvey/repositories/SurveyRepository.java
@@ -4,26 +4,32 @@ import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssurvey.model.Question;
 import com.ssurvey.model.Survey;
 
 @Repository
 public class SurveyRepository {
-	
-	private SessionFactory sessionFactory;
-	
-	public SurveyRepository(){
-	}
-	
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
 
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
-	
-	@Transactional
-	public void saveSurvey(Survey survey){
-		this.sessionFactory.getCurrentSession().save(survey);
-	}
+  private SessionFactory sessionFactory;
+
+  public SurveyRepository() {
+  }
+
+  public SessionFactory getSessionFactory() {
+    return sessionFactory;
+  }
+
+  public void setSessionFactory(SessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
+  }
+
+  @Transactional
+  public void saveSurvey(Survey survey) {
+    this.sessionFactory.getCurrentSession().save(survey);
+  }
+
+  @Transactional
+  public void saveQuestion(Question question) {
+    this.sessionFactory.getCurrentSession().save(question);
+  }
 }
--- modules/ssurvey-webapp/src/main/java/com/ssurvey/service/SurveyService.java
+++ modules/ssurvey-webapp/src/main/java/com/ssurvey/service/SurveyService.java
@@ -1,24 +1,30 @@
 package com.ssurvey.service;
 
+import com.ssurvey.model.Question;
 import com.ssurvey.model.Survey;
 import com.ssurvey.repositories.SurveyRepository;
 
 public class SurveyService {
-	private SurveyRepository surveyRepository;
-	
-	public SurveyService(){	
-	}
-	
-	public SurveyRepository getSurveyRepository() {
-		return surveyRepository;
-	}
 
-	public void setSurveyRepository(SurveyRepository surveyRepository) {
-		this.surveyRepository = surveyRepository;
-	}
-	
-	public void saveSurvey(Survey survey){
-		this.surveyRepository.saveSurvey(survey);
-	}
-	
+  private SurveyRepository surveyRepository;
+
+  public SurveyService() {
+  }
+
+  public SurveyRepository getSurveyRepository() {
+    return surveyRepository;
+  }
+
+  public void setSurveyRepository(SurveyRepository surveyRepository) {
+    this.surveyRepository = surveyRepository;
+  }
+
+  public void saveSurvey(Survey survey) {
+    this.surveyRepository.saveSurvey(survey);
+  }
+
+  public void saveQuestion(Question question) {
+    this.surveyRepository.saveQuestion(question);
+  }
+
 }
