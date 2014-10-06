578e7c82b6cac5da6e52be3cf9ff6b04dcc992c2
Facundo Galán <facug91_lajuve@hotmail.com>
2014-10-06 16:49:57 -0300

    Add redirection to not logged users to /login, and to login users to /surveys/ closes #35

--- modules/ssurvey-webapp/src/main/java/com/ssurvey/web/controllers/HomeController.java
+++ modules/ssurvey-webapp/src/main/java/com/ssurvey/web/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.ssurvey.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ssurvey.service.SurveyService;
+import com.ssurvey.service.signin.SSurveyUser;
 
 @Controller
 public class HomeController extends SSurveyGenericController {
@@ -16,7 +18,12 @@ public class HomeController extends SSurveyGenericController {
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public ModelAndView showLogin() {
-    ModelAndView mv = new ModelAndView("login");
-    return mv;
+    if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() != "anonymousUser") {
+      ModelAndView mv = new ModelAndView("redirect:/surveys/");
+      return mv;
+    } else {
+      ModelAndView mv = new ModelAndView("login");
+      return mv;
+    }
   }
 }
